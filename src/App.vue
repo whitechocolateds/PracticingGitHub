2c815677feccefbd68bd1fa284cbfdb1b210cba5
David Stupar <stupardavid3@gmail.com>
2023-05-22 16:08:58 +0200

    Inicijalizacija projekta. Dodane prve datoteke

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+   <v-btn density="compact" flat="false" height="8rem" color="green">
+  Button
+</v-btn>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+.div{
+  width: 200px;
+  height: 150px;
+  border: 2px solid black;
+  text-align: center;
+  
+}
+</style>
